4ba199ea04869c47bd8c27fa1687f889a886c869
sebai <sebai@DESKTOP-J753RRL>
2017-06-25 00:42:58 -0400

    Metodos clase producto

--- lib/Producto.php
+++ lib/Producto.php
@@ -2,5 +2,61 @@
 
 
 class Producto {
-    //put your code here
+    var $nombre;
+    var $nombreImagen;
+    var $ubicacionImagen;
+    
+    function __construct($nom="",$nomImg="",$ubiImg=""){
+        $this->nombre=$nom;
+        $this->nombreImagen=$nomImg;
+        $this->ubicacionImagen=$ubiImg;
+    }
+    
+    function __construct2($nom=""){
+        $this->nombre=$nom;   
+    }
+            
+    function AgregarProducto(){
+        $oConn=new Conexion();
+        
+        if ($oConn->Conectar()) {
+            $db = $oConn->objconn; 
+        } else {return false;}
+        
+        if ($this->VerificaProducto()){
+            return false;   
+        }  
+        else if($this->nombreImagen !== "" && $this->ubicacionImagen !== "")
+        {
+            $sql="INSERT INTO producto (nombre,nombreImagen,ubicacionImagen) VALUES ('$this->nombre','$this->nombreImagen','$this->ubicacionImagen')";
+            $db->query($sql);
+            return true;
+        }
+        else
+        {
+            $sql="INSERT INTO producto (nombre) VALUES ('$this->nombre')";
+            $db->query($sql);
+            return true;
+        }
+    }
+    
+    function VerificaProducto(){
+        $oConn=new Conexion();
+        
+        if ($oConn->Conectar()) {
+            $db = $oConn->objconn;
+        } else {
+            return false;
+        }
+
+        $sql="SELECT * FROM producto WHERE nombre='$this->nombre'";
+        
+        $resultado=$db->query($sql);
+               
+        if ($resultado->num_rows >= 1) {
+            return true;
+        } else {
+            return false;
+        }
+    }
 }
